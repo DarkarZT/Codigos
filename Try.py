fc36d973a3cd97942cf1391af3da0201a45aa1d4
DarkarZT <116039912+DarkarZT@users.noreply.github.com>
2022-10-18 12:57:31 -0500

    Add files via upload

---
+++ Try.py
@@ -0,0 +1,88 @@
+"""valor_beca = float(input())
+
+if valor_beca >= 4.5:
+    print ("Tu promedio aplica para la beca de 800.000")
+else:
+    print("tu promedio no alcanza para la beca pero tienes bono de 500.000")"""
+
+#######################################################################
+"""Carnet, Cargo Fijo, Cargo Variable, y Total a pagar de matricula"""
+
+"""Carnet = input("Ingrese Su Carnet ")
+Estrato = int(input("Ingrese su Estrato "))
+NumerodeCreditos = int(input("ingrese cantidad de Creditos "))
+Cargo_Fijo = 900000
+Valor_deCredito = 100000
+ValorCreditoVaria = Valor_deCredito * NumerodeCreditos
+
+if Estrato >= 4 and Estrato <= 6:
+   Total_Matricula = ValorCreditoVaria + Cargo_Fijo
+
+if Estrato >= 1 and Estrato <= 3 :
+    Total_Matricula = Cargo_Fijo
+
+print(Total_Matricula)
+print(Carnet)
+print(Cargo_Fijo)
+print(ValorCreditoVaria)"""
+
+######################################################################
+""""
+x = 10
+y = 10
+z = "10"
+
+if (x == y):
+    print("Unooo")
+if (y == int(z)):
+    print("dooooos")
+else:
+    if(x == y):
+        print("Tres")
+    else:
+        print("Cuatro")
+
+#La respuesta del codigo es Unooo, dooooos 
+
+"""
+
+######################################################################
+
+"""Año_entrada = int(input("digite año"))
+
+if (Año_entrada % 4 == 0 and Año_entrada % 100 != 0) or Año_entrada % 400 == 0:
+
+    print("Es año bisiesto")
+else:
+   print("no es bisiesto")"""
+
+#######################################################################
+
+"""usuario = input("Ingrese Usuario")
+Contraseña = input("ingrese contraseña")"""
+
+
+
+
+#######################################################################
+
+#Muestra si el valor de A corresponde a los numeros bajo codigo
+#2 el  valor si definimos A como 20 es "Valor de A es 20 " y "Valor Exacto de A es 20"
+#el  valor si definimos A como 40 es "Valor de A es 40 " y "Valor Exacto de A es 40"
+#el  valor si definimos A como 30 es "Valor de A es 30 " y "Valor Exacto de A es 30"
+#"Valor Exacto de A es 40"
+""""
+a = int(input(" Ingrese vlaor de a positivo "))
+if( a == 10 ):
+ print("Value of a is 10\n" )
+else:
+    if( a == 20 ):
+        print("Value of a is 20\n" )
+
+    elif ( a == 30 ):
+        print("Value of a is 30\n" )
+    else :
+        print("None of the values is matching\n" )
+
+print("Exact value of a is: ", a )"""
+
